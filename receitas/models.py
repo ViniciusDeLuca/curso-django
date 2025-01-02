d2ec4353f06498e5fd8bb73520145f67d0affe0c
vinicius.luca <vinicius.luca@itec.al.gov.br>
2025-01-02 13:56:25 -0300

    Cria migration de receita

--- receitas/models.py
+++ receitas/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Receita(models.Model):
+    titulo = models.CharField(max_length=120)
+    descricao = models.CharField(max_length=165)
+    tempo_preparo = models.IntegerField()
+    unidade_preparo = models.CharField(max_length=50)
+    unidades_porcao = models.IntegerField()
+    etapas_preparo = models.TextField()
+    etapas_preparo_is_html = models.BooleanField(default=False)
+    publicado = models.BooleanField(default=True)
+    capa = models.ImageField(upload_to='receitas/capas/%Y/%m/%d/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
