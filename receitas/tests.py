2401625a2ee655ba65bec621fc5b6782976868de
vinicius.luca <vinicius.luca@itec.al.gov.br>
2025-01-07 10:25:20 -0300

    feat(teste): Testa uma rota passando parametros

--- receitas/tests.py
+++ receitas/tests.py
@@ -1,7 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
 
 class ReceitaURLsTest(TestCase):
-    def test_the_pytest_is_ok(self):
-        variavel = 123
-        print(variavel)
-        assert 1 == 1, 'Está correto'
+    def test_inicio_url_esta_correto(self):
+        url = reverse('inicio')
+        print(url)
+        self.assertEqual(url, '/')
+    
+    def test_categoria_receita_url_esta_correto(self):
+        url = reverse('receitas_por_categorias', kwargs={'categoria_id': 1})
+        print(url)
+        self.assertEqual(url, '/receita/categoria/1/')
