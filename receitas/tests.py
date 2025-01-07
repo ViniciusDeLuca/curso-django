48d2435fa4cccdc6f8bdd76ae42c19b9717a2fff
vinicius.luca <vinicius.luca@itec.al.gov.br>
2025-01-07 09:18:08 -0300

    feat(teste-unitario): Testa a função dentro da classe criada

--- receitas/tests.py
+++ receitas/tests.py
@@ -2,4 +2,6 @@ from django.test import TestCase
 
 class ReceitaURLsTest(TestCase):
     def test_the_pytest_is_ok(self):
-        ...
+        variavel = 123
+        print(variavel)
+        assert 1 == 1, 'Está correto'
