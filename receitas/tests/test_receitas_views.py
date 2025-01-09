4c22d9a54668209ea4f4a88b70a6f436c5947e2d
vinicius.luca <vinicius.luca@itec.al.gov.br>
2025-01-09 15:39:45 -0300

    feat(testes-unitarios): Adiciona testes para as funções de busca de receitas

--- receitas/tests/test_receitas_views.py
+++ receitas/tests/test_receitas_views.py
@@ -114,3 +114,24 @@ class ReceitaViewsTest(ReceitaBaseTest):
         response = self.client.get(reverse('receita_detalhe', kwargs={'id': 2}))
         self.assertEqual(response.status_code, 404)
         
+    def test_busca_de_receita_utiliza_a_funcao_correta(self):
+        resolved = resolve(reverse('busca'))
+        self.assertIs(resolved.func, views.busca)
+        
+    def test_carrega_o_template_correto_para_busca_de_receita(self):
+        response = self.client.get(reverse('busca'))
+        self.assertTemplateUsed(
+            response,
+            'paginas/busca.html')
+    
+    def test_url_busca_retorna_404_com_termo_vazio(self):
+        response = self.client.get(reverse('busca'))
+        self.assertEqual(response.status_code, 404)
+        
+    def test_valor_de_busca_esta_escapado(self):
+        url = reverse('busca') + '?q=teste'
+        response = self.client.get(url)
+        self.assertIn(
+            'Busca por "teste"',
+            response.content.decode('utf-8')
+        )
