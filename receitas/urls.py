8aaa99ccfeaf3906c1369a88c27a41b0b83913d7
vinicius.luca <vinicius.luca@itec.al.gov.br>
2025-01-09 15:38:25 -0300

    feat(url-busca):  Adiciona url para busca de receitas

--- receitas/urls.py
+++ receitas/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio, name="inicio"),
+    path('receita/busca/', views.busca, name="busca"),
     path('receita/categoria/<int:categoria_id>/', views.receitaCategoria, name="receitas_por_categorias"),
     path('receita/<int:id>/', views.receitaDetalhe, name="receita_detalhe"),
 ]
