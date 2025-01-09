07b840fb211ecaeaf1344900fb01113c130f8f9c
vinicius.luca <vinicius.luca@itec.al.gov.br>
2025-01-09 15:40:15 -0300

    feat(funcao-busca): Adiciona função para busca de receitas (WIP)

--- receitas/views.py
+++ receitas/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_list_or_404, get_object_or_404
 from utils.receitas.factory import gerar_receita_ficticia
 from pprint import pprint
 from receitas.models import Receita, Categoria
+from django.http import Http404
 
 def inicio(request):
     receitas = Receita.objects.all().order_by('-id')
@@ -30,4 +31,14 @@ def receitaCategoria(request, categoria_id):
         'receitas': receitas,
         'categoria': receitas[0].categoria.nome,
         'pagina_detalhe': False
+    })
+    
+def busca(request):
+    valor_busca = request.GET.get('q', '').strip()
+    if not valor_busca:
+        raise Http404()
+    return render(request, 'paginas/busca.html', context={
+        'pagina_detalhe': False,
+        'titulo_pagina': f'Busca por "{valor_busca}" | ',
+        'valor_busca': valor_busca
     })
